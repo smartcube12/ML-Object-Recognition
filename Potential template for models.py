2dac7627de2eba9c83a8a814a06c22f6c7914e3e
smartcube12 <twalker25@unl.edu>
2026-04-04 13:36:51 -0500

    Worked on convo and made a template

---
+++ Potential template for models.py
@@ -0,0 +1,61 @@
+import sys
+import tensorflow as tf
+from keras import layers
+#import keras as keras?
+import numpy as np
+import pandas as pd
+import os
+os.environ["KERAS_BACKEND"] = "tensorflow"
+#here they would have scikit and import train_test_split i dont think we need it
+
+
+#----fetching data----- (may be another file)
+
+
+# -------Defining the model ------------ 
+
+#From book these keras calls might need to be layers for our current import without 
+#line 4
+CNN = keras.Sequential(
+    [
+        keras.layers.Input(shape=(784,)),
+        # Hidden layer 1 = 256 nodes, linear activation
+        keras.layers.Dense(256, activation="linear"),
+        # Hidden layer 2: 128 nodes, linear activation
+        keras.layers.Dense(128, activation="linear"),
+        # Output layer: 10 nodes, one per class
+        keras.layers.Dense(10, activation="softmax"),
+    ]
+)
+CNN.summary()
+
+
+# -------Training the model ------------ Table 6.4.3
+
+# model.compile(
+#     #all are example choices use is not required
+#     optimizer = "rmsprop",
+#     loss = "SparseCategoricalCrossentropy",
+#     metrics=["accuracy"],
+# )
+
+
+#model.fit(Xtrain, ytrain, batch_size=64, epochs=10)
+# ^ should be all we need
+
+# Can do the below code to help with evaluation but we could honnestly try the code below that 
+# Book says: training =model.fit(Xtrain, ytrain, batch_size=64, epochs=10) \n  training.history
+#We could do: model.history
+
+
+# -------Evaluating the model ------------ 
+#results = model.evaluate(Xtest, ytest, batch_size=64)
+#print("Test loss, accuracy", results)
+# predictions = model.predict(Xtest[:3])
+# print("Predictions:", predictions.round(3))
+# print("Actual values:", ytest[:3])
+
+
+
+
+print("Hello, world 2")
